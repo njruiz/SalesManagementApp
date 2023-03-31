3dfee4f22ebe36dbff38b44d6a97eb5ad8e27332
Nelson Ruiz <njruiz.nr@gmail.com>
2023-03-31 16:09:09 +0800

    feat: Dashboard Header layout

--- Client/src/app/components/header/header.component.ts
+++ Client/src/app/components/header/header.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
+  firstName = "Nelson";
+  lastName = "Ruiz Jr.";
+  dashboard_subMessage = "Here are today's stats from your store";
 }
