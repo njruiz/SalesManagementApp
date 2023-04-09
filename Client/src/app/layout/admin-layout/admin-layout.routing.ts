dfa36ea64f89f9f1aef094d4f20cbd63986258db
Nelson Ruiz <njruiz.nr@gmail.com>
2023-04-09 18:33:30 +0800

    Feat: Add header
    Add header to the ff components:
    - Inventory
    - Reports
    - Settings
    
    Add constructors and implement OnInit

--- Client/src/app/layout/admin-layout/admin-layout.routing.ts
+++ Client/src/app/layout/admin-layout/admin-layout.routing.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { ProductsComponent } from 'src/app/components/products/products.component';
 import { DashboardComponent } from 'src/app/components/dashboard/dashboard.component';
 import { OrdersComponent } from 'src/app/components/orders/orders.component';
+import { InventoryComponent } from 'src/app/components/inventory/inventory.component';
+import { ReportsComponent } from 'src/app/components/reports/reports.component';
+import { SettingsComponent } from 'src/app/components/settings/settings.component';
 
 export const AdminLayoutRoutes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'orders', component: OrdersComponent },
   { path: 'products', component: ProductsComponent },
+  { path: 'inventory', component: InventoryComponent },
+  { path: 'reports', component: ReportsComponent },
+  { path: 'settings', component: SettingsComponent },
 ];
