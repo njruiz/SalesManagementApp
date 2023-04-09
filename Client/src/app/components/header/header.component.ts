0526e31922e3afb9d447d6aaa940a2ad37bd5627
Nelson Ruiz <njruiz.nr@gmail.com>
2023-04-09 17:46:46 +0800

    feat: Added header to components
    Added header on the following components:
    - dashboard, orders, products

--- Client/src/app/components/header/header.component.ts
+++ Client/src/app/components/header/header.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-  firstName = "Nelson";
-  lastName = "Ruiz Jr.";
+export class HeaderComponent implements OnInit {
+  @Input() screen_name: string;
+  dashboardMode = false;
+
+  firstName = 'Nelson';
+  lastName = 'Ruiz Jr.';
   dashboard_subMessage = "Here are today's stats from your store";
+
+  ngOnInit(): void {
+    if (this.screen_name == 'Dashboard') {
+      this.dashboardMode = true;
+    }
+  }
 }
