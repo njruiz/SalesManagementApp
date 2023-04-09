915352770059b2490cf9c6f458e405d19f83b94b
Nelson Ruiz <njruiz.nr@gmail.com>
2023-04-09 17:49:09 +0800

    feat: Added new components
    New components:
    - inventory
    - reports
    - settings

--- Client/src/app/components/components.module.ts
+++ Client/src/app/components/components.module.ts
@@ -7,9 +7,12 @@ import { AngularMaterialModule } from '../angular-material.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { OrdersComponent } from './orders/orders.component';
 import { NavComponent } from './nav/nav.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { ReportsComponent } from './reports/reports.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
-  declarations: [DashboardComponent, OrdersComponent, ProductsComponent, SidebarComponent, NavComponent],
+  declarations: [DashboardComponent, OrdersComponent, ProductsComponent, SidebarComponent, NavComponent, InventoryComponent, ReportsComponent, SettingsComponent],
   imports: [CommonModule, RouterModule, AngularMaterialModule],
   exports: [SidebarComponent],
 })
