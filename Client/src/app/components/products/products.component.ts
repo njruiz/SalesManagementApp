c9d5b1bbebd06fff4d03c3998398aa8e2664028f
Nelson Ruiz Jr <nelson.ruiz@fujitsu.com>
2023-06-09 14:23:13 +0800

    Squashed commit of the following:
    commit 46459ea6ac8be648b9c473ff216fbc7de7ec30b6
    Merge: 0c34c5e f189aab
    Author: Nelson <njruiz.nr@gmail.com>
    Date:   Fri Jun 9 14:13:02 2023 +0800
    
        Merge pull request #5 from njruiz/ISMS-13-Products-UI
    
        Product table adjustments
    
    commit f189aab3475b92c3ee7f3fc800672cdd04412246
    Author: Nelson Ruiz Jr <nelson.ruiz@fujitsu.com>
    Date:   Wed Jun 7 14:15:01 2023 +0800
    
        Product table adjustments
    
        - Remove Status and Stocks in products
        - Align center elements of table

--- Client/src/app/components/products/products.component.ts
+++ Client/src/app/components/products/products.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
-import { Sort } from '@angular/material/sort';
+import { Component, ViewChild } from '@angular/core';
+import { MatSort, Sort } from '@angular/material/sort';
 import { AddProductModalComponent } from '../modals/add-product-modal/add-product-modal.component';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface Products {
   name: string;
   category: string;
-  status: string;
-  stocks: number;
   price: number;
   id: string;
 }
@@ -18,44 +17,36 @@ export interface Products {
   styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
+  displayedColumns = ['id', 'name', 'category', 'price', 'actions'];
+  dataSource: MatTableDataSource<Products>;
   products: Products[] = [
     {
       name: 'Dark Chocolate IceCream Cake',
       category: 'IceCream Cake',
-      status: 'Available',
-      stocks: 31,
       price: 135,
       id: '202301',
     },
     {
       name: 'Coffee IceCream Cake',
       category: 'IceCream Cake',
-      status: 'Low Stocks',
-      stocks: 5,
       price: 135,
       id: '202302',
     },
     {
       name: 'Ube Keso IceCream Cake',
       category: 'IceCream Cake',
-      status: 'Sold Out',
-      stocks: 0,
       price: 135,
       id: '202303',
     },
     {
       name: 'Strawberry Choco Cone Bites',
       category: 'Cone Bites',
-      status: 'Available',
-      stocks: 123,
       price: 135,
       id: '202304',
     },
     {
       name: 'Red Velvet Cone Bites',
       category: 'Cone Bites',
-      status: 'Available',
-      stocks: 15,
       price: 135,
       id: '202305',
     }
@@ -63,8 +54,41 @@ export class ProductsComponent {
 
   sortedData: Products[];
 
+  @ViewChild(MatSort) sort: MatSort;
+
   constructor(public dialog: MatDialog) {
-    this.sortedData = this.products.slice();
+    this.dataSource = new MatTableDataSource([{
+      id: '202301',
+      name: 'Dark Chocolate IceCream Cake',
+      category: 'IceCream Cake',
+      price: 1335.00,
+    },
+    {
+      id: '202302',
+      name: 'Coffee IceCream Cake',
+      category: 'IceCream Cake',
+      price: 135.00,
+    },
+    {
+      id: '202303',
+      name: 'Ube Keso IceCream Cake',
+      category: 'IceCream Cake',
+      price: 135.00,
+    },
+    {
+      id: '202304',
+      name: 'Strawberry Choco Cone Bites',
+      category: 'Cone Bites',
+      price: 135.00,
+    },
+    {
+      id: '202305',
+      name: 'Red Velvet Cone Bites',
+      category: 'Cone Bites',
+      price: 135.00,
+    }]);
+
+    this.sortedData = this.dataSource.data.slice();
   }
 
   addNewProduct(): void {
@@ -95,27 +119,23 @@ export class ProductsComponent {
   }
 
   sortData(sort: Sort) {
-    const data = this.products.slice();
+    const data = this.dataSource.data.slice();
     if (!sort.active || sort.direction === '') {
       this.sortedData = data;
       return;
     }
 
     this.sortedData = data.sort((a, b) => {
-      const isAsc = sort.direction === 'asc';
+      const isAsc = sort.direction === "asc";
       switch (sort.active) {
-        case 'name':
-          return compare(a.name, b.name, isAsc);
-        case 'category':
-          return compare(a.category, b.category, isAsc);
-        case 'status':
-          return compare(a.status, b.status, isAsc);
-        case 'stocks':
-          return compare(a.stocks, b.stocks, isAsc);
-        case 'price':
-          return compare(a.price, b.price, isAsc);
-        case 'id':
+        case "id":
           return compare(a.id, b.id, isAsc);
+        case "name":
+          return compare(a.name, b.name, isAsc);
+        case "category":
+          return compare(a.category, b.category, isAsc);
+        case "price":
+          return compare(a.price, b.price, isAsc);
         default:
           return 0;
       }
