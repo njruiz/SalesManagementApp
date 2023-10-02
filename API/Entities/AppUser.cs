4cc5ee37d99b16694ceea909b317a925a4013f4a
Nelson Ruiz <njruiz.nr@gmail.com>
2023-10-02 17:38:36 +0800

    feat: Code optimization
    - Use automapper
    - Seed users

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,6 +6,11 @@ public class AppUser
     public string UserName { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public string Address { get; set; }
+    public string ContactNumber { get; set; }
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+    public List<Photo> Photos { get; set; } = new();
 }
